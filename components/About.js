4022eec46b051b43140a64ee2723d87287325d5e
ihsanrabb <ihsanrabbani25@gmail.com>
2020-09-30 10:59:43 +0700

    add id section & set resume link

--- components/About.js
+++ components/About.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 
 const About = () => {
   return (
-    <div className="about-wrap">
+    <div className="about-wrap" id="about-section">
       <Container maxWidth="md" className="about-container">
         <Grid container spacing={2}>
           <Grid item xs={12}>
@@ -15,7 +15,7 @@ const About = () => {
           </Grid>
           <Grid item xs={12} md={6}>
             <p className="about-wording">I'm Graduate Information System student with experience in JavaScript with use framework & libraries such as ReactJS and VueJs. A fast learner and decenet managing thing. Reliable team player who want to help by improving the application</p>
-            <Button variant="outlined" color="secondary" size="medium">
+            <Button variant="outlined" color="secondary" size="medium" href="https://firebasestorage.googleapis.com/v0/b/sadari-covid.appspot.com/o/news%2F1601438229081CV%20Ihsanuddin%20Rabbani.pdf?alt=media&token=8577b701-62d1-410e-82e8-ccadb24d9513" target="_blank">
               View resume
             </Button>
           </Grid>
