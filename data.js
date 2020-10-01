cabc802f4cf2e1d052a80c9f29390cf2bdd92ddd
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-01 12:50:24 +0700

    update sadari covid data

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 export const projectsData = [
     {
         title: "SADARI | Covid 19",
-        description: "Cheflicious is a recipe sharing website for users to publish their own recipe. Users can signup and sign in to the app and be able to post their recipe. It also has a follow system where users can follow other user and vice versa. This project is created with Nextjs for server side rendering, static site generation, and for better SEO. For the backend, I use Nodejs and express to create the RESTful API, and uses MongoDB as database and AWS S3 for storing images. For realtime communication i use socket.io to send notification to user whenever other user commented or liked their recipe.",
-        imgURL: "/hajj-shop.png",
+        description: "SADARI is a web app for users to see lastest update about Covid 19 infortmation in Indonesia. Build with NEXTJS and Material UI.",
+        imgURL: "/sadari-covid.png",
         demoURL: "https://sadari-covid.vercel.app/",
         repoURL: "https://github.com/ihsanrabb/sadari-covid"
     },
