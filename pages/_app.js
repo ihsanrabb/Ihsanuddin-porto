d9fb3cb1c37ef3e563e8b7408c9b0fc0ea8e73d2
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-13 22:05:36 +0700

    remove animate css CDN

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,6 @@ function MyApp({ Component, pageProps }) {
         <Head>
           <title>Ihsanuddin | Frontend Developer</title>  
           <link defer rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-          <link defer rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
         </Head>
         <CssBaseline />
        
