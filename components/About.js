77c22b22f1f741f01fe1029546b7ec21986abe66
ihsanrabbani <ihsanrabbani25@gmail.com>
2021-05-15 22:04:51 +0700

    remove react reveal animation

--- components/About.js
+++ components/About.js
@@ -11,7 +11,11 @@ const About = () => {
           <h2 className="about-title">ABOUT ME</h2>   
           </Grid>
           <Grid item xs={12} md={6}>
-            <img src="/ihsan-avatar1.jpg" alt="Avatar" className="about-img" />
+            <img 
+              src="/ihsan-avatar1.jpg" 
+              alt="ihsanuddin rabbani" 
+              className="about-img" 
+            />
           </Grid>
           <Grid item xs={12} md={6}>
             <p className="about-wording">I'm Graduate Information System student with experience in JavaScript with use framework & libraries such as ReactJS and VueJs. A fast learner and decenet managing thing. Reliable team player who want to help by improving the application</p>
--- components/Projects.js
+++ components/Projects.js
@@ -2,37 +2,32 @@ import {projectsData} from '../data'
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-import Fade from 'react-reveal/Fade';
 
 const Projects = () => {
   return (
     <>
       <h1 className="text-center">Projects</h1>
-     
-          <Container maxWidth="lg">
-            {projectsData.map((project, index) => (
-              <Fade left key={index}>
-                <Grid container spacing={4} className="projects-wrap">
-                  <Grid item xs={12} md={5}>
-                    <h2>{project.title}</h2>
-                    <p>{project.description}</p>
-                    <Button variant="contained" color="primary" href={project.demoURL} target="_blank">
-                      See Live
-                    </Button>
-                    <Button variant="outlined" color="primary" className="ml-2" href={project.repoURL} target="_blank">
-                      Source code
-                    </Button>
-                  </Grid>
-                  <Grid item xs={12} md={7} container direction="row" justify="center" alignItems="center">
-                    <div className="projects-thumbnail">
-                      <img src={project.imgURL} alt="Avatar" className="img-fluid" />  
-                    </div>
-                  </Grid>
-                </Grid>
-              </Fade>
-            ))}
-          </Container>
-     
+      <Container maxWidth="lg">
+        {projectsData.map((project, index) => (
+          <Grid container spacing={4} className="projects-wrap" key={index}>
+            <Grid item xs={12} md={5}>
+              <h2>{project.title}</h2>
+              <p>{project.description}</p>
+              <Button variant="contained" color="primary" href={project.demoURL} target="_blank">
+                See Live
+              </Button>
+              <Button variant="outlined" color="primary" className="ml-2" href={project.repoURL} target="_blank">
+                Source code
+              </Button>
+            </Grid>
+            <Grid item xs={12} md={7} container direction="row" justify="center" alignItems="center">
+              <div className="projects-thumbnail">
+                <img src={project.imgURL} alt="Avatar" className="img-fluid" />  
+              </div>
+            </Grid>
+          </Grid>
+        ))}
+      </Container>
     </>
   )
 }
